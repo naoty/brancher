4ebe3fc59aaed2693efbf4128c14dbfe356495f7
chaunce <chaunce.slc@gmail.com>
2015-08-18 08:39:47 -0600

    cap database name length
    cap database name length to 63 characters, this conforms to mysql and postgres default limitations for name lengths.

--- lib/brancher/database_rename_service.rb
+++ lib/brancher/database_rename_service.rb
@@ -9,7 +9,7 @@ module Brancher
       database_extname = File.extname(configuration["database"])
       database_name = configuration["database"].gsub(%r{#{database_extname}$}) { "" }
       database_name += suffix unless database_name =~ %r{#{suffix}$}
-      configuration["database"] = database_name + database_extname
+      configuration["database"] = cap_length(database_name + database_extname)
       configurations
     end
 
@@ -21,6 +21,12 @@ module Brancher
     end
 
     private
+
+    def cap_length(database_full_name)
+      max_length = 63
+      database_full_name = database_full_name.slice(0,max_length-22) + [Digest::MD5.digest(database_full_name)].pack("m0").slice(0,22) if database_full_name.length > max_length
+      database_full_name
+    end
 
     def env
       Rails.env
