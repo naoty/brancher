93ac9a6c5013578ddee28801f3fecb09004feb4e
chaunce <chaunce.slc@gmail.com>
2015-08-18 09:20:19 -0600

    use database_name_with_suffix instead of suffix

--- lib/brancher/database_rename_service.rb
+++ lib/brancher/database_rename_service.rb
@@ -6,25 +6,26 @@ module Brancher
 
     def rename!(configurations)
       configuration = configurations[env]
+      configuration["database"] = database_name(configuration)
+      configurations
+    end
+
+    def database_name_with_suffix(configuration)
       database_extname = File.extname(configuration["database"])
       database_name = configuration["database"].gsub(%r{#{database_extname}$}) { "" }
       database_name += suffix unless database_name =~ %r{#{suffix}$}
-      configuration["database"] = cap_length(database_name + database_extname)
-      configurations
+      database_name += database_extname
+      database_name = database_name.slice(0,Brancher.config.max_database_name_length-22) + [Digest::MD5.digest(database_name)].pack("m0").slice(0,22) if database_name.length > Brancher.config.max_database_name_length
+      database_name
     end
+
+    private
 
     def suffix
       return nil if current_branch.blank?
       return nil if Brancher.config.except_branches.include?(current_branch)
 
       "_#{current_branch}"
-    end
-
-    private
-
-    def cap_length(database_name)
-      database_name = database_name.slice(0,Brancher.config.max_database_name_length-22) + [Digest::MD5.digest(database_name)].pack("m0").slice(0,22) if database_name.length > Brancher.config.max_database_name_length
-      database_name
     end
 
     def env
