70d10f74e336465209fb2a86e0e810b5474a2c10
chaunce <chaunce.slc@gmail.com>
2015-08-20 09:01:46 -0600

    Add test for max_database_name_length

--- lib/brancher/database_rename_service.rb
+++ lib/brancher/database_rename_service.rb
@@ -7,15 +7,15 @@ module Brancher
     def rename!(configurations)
       configuration = configurations[env]
       configuration["original_database"] = configuration["database"]
-      configuration["database"] = database_name(configuration)
+      configuration["database"] = database_name_with_suffix(configuration["database"])
       configurations
     end
 
     private
 
-    def database_name_with_suffix(configuration)
-      database_extname = File.extname(configuration["database"])
-      database_name = configuration["database"].gsub(%r{#{database_extname}$}) { "" }
+    def database_name_with_suffix(database)
+      database_extname = File.extname(database)
+      database_name = database.gsub(%r{#{database_extname}$}) { "" }
       database_name += suffix unless database_name =~ %r{#{suffix}$}
       database_name += database_extname
       database_name = database_name.slice(0,Brancher.config.max_database_name_length-22) + [Digest::MD5.digest(database_name)].pack("m0").slice(0,22) if database_name.length > Brancher.config.max_database_name_length
@@ -23,9 +23,7 @@ module Brancher
     end
 
     def suffix
-      return nil if current_branch.blank?
-      return nil if Brancher.config.except_branches.include?(current_branch)
-
+      return nil if current_branch.blank? || Brancher.config.except_branches.include?(current_branch)
       "_#{current_branch}"
     end
 
